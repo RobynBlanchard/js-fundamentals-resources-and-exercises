5fe689809042e5ff425787b7dd9f65ab9ee3790e
Petter Franzen <petterfranzen@notonthehighstreet.com>
2019-01-30 14:44:14 +0000

    Comments on promises

--- promises/exercises.js
+++ promises/exercises.js
@@ -1,18 +1,27 @@
-/****************
- * Promises
- */
+// /****************
+//  * Promises
+//  */
 
-// Create a function that returns a promise, this promise will resolve after 1000 ms
-// - Function returns a resolved promise and is handled by the consuming function
-import { setTimeout } from 'timers';
+// // Create a function that returns a promise, this promise will resolve after 1000 ms
+// // - Function returns a resolved promise and is handled by the consuming function
+// // import { setTimeout } from 'timers'; - Not required, setTimeout is global
 
-const newPromise = () => {
-  return new Promise(function(resolve) {
+// Standard promise, looks fine, can be done without the brackets
+// const newPromise = () => {
+//   return new Promise(function(resolve) {
+//     setTimeout(function() {
+//       return resolve("success");
+//     }, 1000);
+//   });
+// };
+
+// Like so
+const newPromise = () =>
+  new Promise(function(resolve) {
     setTimeout(function() {
-      return resolve('success');
+      return resolve("success");
     }, 1000);
   });
-};
 
 newPromise().then(res => console.log(res));
 
@@ -23,11 +32,18 @@ const newPromise2 = () => {
   return new Promise(function(resolve, reject) {
     setTimeout(function() {
       const result = Math.random(0, 2).toFixed(0);
-      if (result === '1') {
-        return resolve('success');
+      // if you use if/else here, you don't need to return
+      if (result === "1") {
+        resolve("success");
       } else {
-        return reject('failure');
+        reject("failure");
       }
+
+      // if it had been without the else, you would've had to stop execution with a return
+      // if (result === "1") {
+      //   return resolve("success");
+      // }
+      // reject("failure");
     }, 1000);
   });
 };
@@ -43,78 +59,112 @@ newPromise2()
 // - Create a chain of 5 promises, where the last handler is a finally block that will always(?) run
 
 const allProducts = {
-  clothes: ['shoe', 'top'],
-  cards: ['birthday', 'anniversary'],
-  tech: ['laptop-cover', 'mousepad'],
-  home: ['wardrobe', 'chair'],
-  jewellery: ['neckalce', 'bracelet']
+  clothes: ["shoe", "top"],
+  cards: ["birthday", "anniversary"],
+  tech: ["laptop-cover", "mousepad"],
+  home: ["wardrobe", "chair"],
+  jewellery: ["neckalce", "bracelet"]
 };
 
 const fetchProducts = category => {
   return new Promise(function(resolve, reject) {
     setTimeout(function() {
       if (allProducts[category]) {
+        console.log(`fetchProducts resolving with ${allProducts[category]}`);
         return resolve(allProducts[category]);
       }
-      return reject('invalid category');
+      return reject("invalid category");
     }, 1000);
   });
 };
 
-fetchProducts('cards')
+// This kind of "callback hell" is the reason promises were invented, returning the promise enables the chain
+// fetchProducts("cards")
+//   .then(productsCards => {
+//     console.log(productsCards);
+//     fetchProducts("clothes")
+//       .then(productsClothes => {
+//         console.log(productsClothes);
+//         fetchProducts("tech")
+//           .then(productsTech => {
+//             console.log(productsTech);
+//             fetchProducts("home")
+//               .then(productsHome => {
+//                 console.log(productsHome);
+//                 fetchProducts("jewellery")
+//                   .then(productsJewellery => {
+//                     console.log(productsJewellery);
+//                   })
+//                   .catch(err => console.log(err))
+//                   .finally(() => console.log("finished fetching cards"));
+//               })
+//               .catch(err => console.log(err))
+//               .finally(() => console.log("finished fetching clothes"));
+//           })
+//           .catch(err => console.log(err))
+//           .finally(() => console.log("finished fetching tech"));
+//       })
+//       .catch(err => console.log(err))
+//       .finally(() => console.log("finished fetching home"));
+//   })
+//   .catch(err => console.log(err))
+//   .finally(() => console.log("finished fetching jewellery"));
+
+fetchProducts("cards")
   .then(productsCards => {
     console.log(productsCards);
-    fetchProducts('clothes')
-      .then(productsClothes => {
-        console.log(productsClothes);
-        fetchProducts('tech')
-          .then(productsTech => {
-            console.log(productsTech);
-            fetchProducts('home')
-              .then(productsHome => {
-                console.log(productsHome);
-                fetchProducts('jewellery')
-                  .then(productsJewellery => {
-                    console.log(productsJewellery);
-                  })
-                  .catch(err => console.log(err))
-                  .finally(() => console.log('finished fetching cards'));
-              })
-              .catch(err => console.log(err))
-              .finally(() => console.log('finished fetching clothes'));
-          })
-          .catch(err => console.log(err))
-          .finally(() => console.log('finished fetching tech'));
-      })
-      .catch(err => console.log(err))
-      .finally(() => console.log('finished fetching home'));
+    return fetchProducts("clothes");
+  })
+  .then(productsClothes => {
+    console.log(productsClothes);
+    return fetchProducts("tech");
+  })
+  .then(productsTech => {
+    console.log(productsTech);
+    return fetchProducts("home");
+  })
+  .then(productsHome => {
+    console.log(productsHome);
+    return fetchProducts("jewellery");
+  })
+  .then(productsJewellery => {
+    console.log(productsJewellery);
+    return Promise.resolve();
   })
   .catch(err => console.log(err))
-  .finally(() => console.log('finished fetching jewellery'));
+  .finally(() => console.log("finished fetching everything"));
 
 // Create an array of 5 promises, pass the array to Promise.all and handle the outcome
 
-Promise.all([
-  fetchProducts('cards'),
-  fetchProducts('clothes'),
-  fetchProducts('tech'),
-  fetchProducts('home'),
-  fetchProducts('jewellery')
-])
-  .then(products => {
-    console.log(products);
-    // products.forEach(product => console.log(product));
-  })
-  .catch(err => console.log(err));
+// Looks fine, could perhaps be done slightly shorter
+// Also, the result from the promise all is an array, so you've got arrays in an array, would need to flatmap
+Promise.all(
+  ["cards", "clothes", "tech", "home", "jewellery"].map(arg =>
+    fetchProducts(arg)
+  )
+).then(products => console.log(products));
 
-// Create an array of 5 promises, pass the array to Promise.race and see which one finishes first
+// Promise.all([
+//   fetchProducts("cards"),
+//   fetchProducts("clothes"),
+//   fetchProducts("tech"),
+//   fetchProducts("home"),
+//   fetchProducts("jewellery")
+// ])
+//   .then(products => {
+//     console.log(products);
+//     // products.forEach(product => console.log(product));
+//   })
+//   .catch(err => console.log(err));
+
+// // Create an array of 5 promises, pass the array to Promise.race and see which one finishes first
 
 Promise.race([
-  fetchProducts('cards'),
-  fetchProducts('clothes'),
-  fetchProducts('tech'),
-  fetchProducts('home'),
-  fetchProducts('jewellery')
+  fetchProducts("cards"),
+  fetchProducts("clothes"),
+  fetchProducts("tech"),
+  fetchProducts("home"),
+  fetchProducts("jewellery")
 ])
   .then(products => {
     console.log(products);
